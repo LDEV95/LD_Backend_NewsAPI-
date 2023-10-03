3095aba6dee85322c21720dee30587743bfddd38
LDEV95 <lawrencefanutzadavis@gmail.com>
2023-10-03 11:23:43 +0100

    Task 3 completed pending approval

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const express = require("express");
 const app = express();
 const { sendTopics } = require("./controllers/NCNEWS.controllers");
 app.get("/api/topics", sendTopics);
+const endpointsData = require("./endpoints.json");
+
+app.get("/api/", (req, res) => {
+  res.status(200).json(endpointsData);
+});
 
 module.exports = app;
